12bc6df645f4b86c1214c2e9cccc38b8022b3bfb
asdf2033 <ahnahc0712@gmail.com>
2023-12-06 14:06:14 +0900

    김찬하 sts 어노테이션 추가

--- OMG/src/main/java/com/oracle/OMG/controller/ShController.java
+++ OMG/src/main/java/com/oracle/OMG/controller/ShController.java
@@ -3,7 +3,12 @@ package com.oracle.OMG.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Data
+@Slf4j
 public class ShController {
 	@RequestMapping(value = "/")
 	public String main() {
