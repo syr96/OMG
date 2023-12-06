e421ed2a236de2818a9527adc9eddc1ad281e20d
asdf2033 <ahnahc0712@gmail.com>
2023-12-06 16:50:21 +0900

    김찬하 DTO 변경사항 적용

--- OMG/src/main/java/com/oracle/OMG/dto/PurDetail.java
+++ OMG/src/main/java/com/oracle/OMG/dto/PurDetail.java
@@ -5,8 +5,8 @@ import lombok.Data;
 @Data
 public class PurDetail {
 	private String 	pur_date;
-    private int 	code;
     private int 	custcode;
+    private int 	code;
     private int 	qty;
     private int 	price;
 }
