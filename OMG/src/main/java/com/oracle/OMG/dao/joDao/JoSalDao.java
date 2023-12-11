9260f4e9f46c21824dc5b3873a38471877965817
junjoonou <144098665+junjoonou@users.noreply.github.com>
2023-12-11 17:47:23 +0900

    [add] sales/salesSearch

--- OMG/src/main/java/com/oracle/OMG/dao/joDao/JoSalDao.java
+++ OMG/src/main/java/com/oracle/OMG/dao/joDao/JoSalDao.java
@@ -7,5 +7,10 @@ import com.oracle.OMG.dto.SalesDetail;
 public interface JoSalDao {
 
 	List<SalesDetail>       getListSalesInquiry(SalesDetail sales);
+	List<SalesDetail>		searchSalesInquiry(SalesDetail sales);
+	int                     getTotalSalesInquiry();
+	int                     getSearchTotalSalesInquiry();
+	int                     getSortTotalSalesInquiry();
+	List<SalesDetail>       sortSalesInquiry(int sales_status);
 
 }
