af2c9bde0004062812ae3ab0a654937dbac42b2c
bigwise92 <bigwise92@naver.com>
2023-12-18 15:05:46 +0900

    태현 - AOP 로깅 진행중

--- OMG/src/main/java/com/oracle/OMG/service/thService/ThReplyServiceImpl.java
+++ OMG/src/main/java/com/oracle/OMG/service/thService/ThReplyServiceImpl.java
@@ -25,7 +25,7 @@ public class ThReplyServiceImpl implements ThReplyService {
 	@Transactional
 	@Override
 	public int register(Reply rep) {
-		log.info("register..." + rep);
+//		log.info("register..." + rep);
 		
 		nd.updateReplyCnt(rep.getBrd_id(), 1);
 		return rd.insert(rep);
@@ -33,20 +33,20 @@ public class ThReplyServiceImpl implements ThReplyService {
 
 	@Override
 	public Reply get(int rep_id) {
-		log.info("get..." + rep_id);
+//		log.info("get..." + rep_id);
 		return rd.read(rep_id);
 	}
 
 	@Override
 	public int modify(Reply rep) {
-		log.info("modify..." + rep);
+//		log.info("modify..." + rep);
 		return rd.update(rep);
 	}
 
 	@Transactional
 	@Override
 	public int remove(int rep_id) {
-		log.info("remove..." + rep_id);
+//		log.info("remove..." + rep_id);
 		
 		// 해당 댓글의 게시물(brd_id) 조회용
 		Reply reply = rd.read(rep_id);
@@ -58,13 +58,13 @@ public class ThReplyServiceImpl implements ThReplyService {
 
 	@Override
 	public List<Reply> getList(Criteria cri, int brd_id) {
-		log.info("get Reply List of a Board brd_id: " + brd_id);		
+//		log.info("get Reply List of a Board brd_id: " + brd_id);		
 		return rd.GetListWithPaging(cri, brd_id);
 	}
 
 	@Override
 	public ReplyPage getListPage(Criteria cri, int brd_id) {
-		log.info("getListPage");
+//		log.info("getListPage");
 		return new ReplyPage(rd.getCountByBrd_id(brd_id), rd.GetListWithPaging(cri, brd_id));
 	}
 	
